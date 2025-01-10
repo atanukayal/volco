e9ea9140383ced428af60d6540027c80e1f6202b
atanukayal <atanuwork2@gmail.com>
2025-01-10 18:46:44 +0530

    update server

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import dotenv from 'dotenv';
+import morgan from 'morgan';
 import connectDB from './config/db.js';
 
 const app = express();
@@ -9,6 +10,14 @@ dotenv.config();
 
 // connect to db
 connectDB();
+
+app.use(express.json());
+app.use(morgan('dev'));
+
+
+app.use('/api/v1/user', require('./routes/userRoutes.js'));
+app.use('/api/v1/admin', require('./routes/adminRoutes.js'));
+app.use('/api/v1/organisation', require('./routes/orgRoutes.js')); // organisation routes
 
 
 app.get('/', (req, res) => {
