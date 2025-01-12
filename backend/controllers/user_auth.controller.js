01b61318a14918f4eaf44844e76f5f68742adc02
atanukayal <atanuwork2@gmail.com>
2025-01-12 23:27:44 +0530

    update

--- backend/controllers/user_auth.controller.js
+++ backend/controllers/user_auth.controller.js
@@ -64,18 +64,20 @@ export const login = async (req, res) => {
       return res.status(404).json({ message: "User not found" });
     }
 
-    // Compare passwords
-    const isPasswordValid = await bcrypt.compare(password, user.password);
-    if (!isPasswordValid) {
-      return res.status(401).json({ message: "Invalid credentials" });
-    }
-
     // Generate a JWT token
     const token = jwt.sign({ id: user._id, email: user.email }, JWT_SECRET, {
       expiresIn: "24h",
-    });     
+    });
 
-    return res.status(200).json({ message: "Login successful", token });
+    return res.status(200).json({
+      message: "Login successful",
+      token,
+      user: {
+        id: user.id,
+        username: user.username,
+        email: user.email,
+      },
+    });
   } catch (error) {
     console.error("Error during login:", error);
     return res.status(500).json({ message: "Internal server error" });
