73e03bbd8e9f0dac06f0c4d9650e23b4c8021a94
prasangeet <b23ch1033@iitj.ac.in>
2025-01-12 22:52:02 +0530

    update

--- backend/routes/user.route.js
+++ backend/routes/user.route.js
@@ -1,14 +1,14 @@
 import express from 'express';
-import { login,logout } from '../controllers/user_auth.controller';
+import { login, signUp } from '../controllers/user_auth.controller';
 import auth from '../middlewares/authmiddleware';
 
 const router = express.Router();
 
 //routes
 //LOGIN || POST
-router.post("/login", loginController);
+router.post("/login", login);
 
 //REGISTER || POST
-router.post("/register", registerController);
+router.post("/register", signUp);
 
    
