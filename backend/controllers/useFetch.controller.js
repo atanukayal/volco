94a6bc3daf5ab55ace53a6fc00b0d9cc53d2d11f
atanukayal <atanuwork2@gmail.com>
2025-01-15 16:28:14 +0530

    update

---
+++ backend/controllers/useFetch.controller.js
@@ -0,0 +1,28 @@
+
+export const fetchUser = async (req, res) => {
+    try {
+        const user = req.user;
+        
+        if (!user) {
+            return res.status(404).json({ message: "User not found" });
+        }
+    
+        return res.status(200).json({
+            message: "User found",
+            user: {
+                id: user._id,
+                displayName: user.displayName,
+                avatar: user.avatar,
+                bio: user.bio,
+                email: user.email,
+                uid: user.uid,
+                profileSetup: user.profileSetup,
+            },
+        });
+    } catch (error) {
+        console.error("Error fetching user:", error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+
+
+}
